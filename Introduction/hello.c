66087ee7829b94d480b3c191b14c1e50e45e945c
peoplechain <peoplechainlibrary@gmail.com>
2024-07-05 15:12:46 +0530

    content one

---
+++ Introduction/hello.c
@@ -0,0 +1,14 @@
+// Include necessary header files
+#include <stdio.h>
+
+// Main function, entry point of the program
+int main() 
+
+{
+    // Statements inside the main function
+    printf("Hello, Enn World!\n");
+
+    // Return statement indicating successful termination
+    return 0;
+
+}
